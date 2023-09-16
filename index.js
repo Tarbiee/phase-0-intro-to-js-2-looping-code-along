e12136ca24abc2f980b76a8f27259fdcb65a35e0
Tarbiee <dorcusstaby@gmail.com>
2023-09-16 23:45:22 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
 // Code your solutions in this file
+
+function countDown(number){
+    let i=10;
+    while(i>=0 ){
+        console.log(i);
+        i --;
+    }
+
+}
+countDown();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  

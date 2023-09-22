ec44c94c535711cc00f9e8439be4286f7381aaf0
Tarbiee <dorcusstaby@gmail.com>
2023-09-23 00:36:54 +0300

    Added changes

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
-// Code your solutions in this file
+// // Code your solutions in this file
 
-function countDown(number){
-    let i=10;
-    while(i>=0 ){
-        console.log(i);
-        i --;
-    }
+const cards = ["Guadalupe", "Ollie", "Aki"];
 
+  function countDown(number){
+      let i=10;
+      while(i>=0 ){
+          console.log(i);
+          i --;
+     }
+
+  }
+ countDown();
+
+ function writeCards(cards, suprise){
+     let i =0;
+     while(i< cards.length){
+         cards.push(`Thank you, ${cards[i]}, for the wonderful ${suprise} gift!`)
+         i++;
+     }
+    return cards;
 }
-countDown();
+ writeCards(cards);
 
 
 
 
-
-
+// function writeCards(namesArray, eventName) {
+//   let messagesArray = [];
+//   for (let i = 0; i < namesArray.length; i++) {
+//     messagesArray.push(`Thank you, ${namesArray[i]}, for the wonderful ${eventName} gift!`);
+//   }
+//   return messagesArray;
+// }
 
 
 
